1b854764fb2014c7faa3aea539822930dd71af2e
Mohan <mohans8086@gmail.com>
2025-08-10 22:56:13 -0500

    image version updated

--- 18-Azure-VM-ScaleSets-Manual-scaling/terraform-manifests/c7-03-web-linux-vmss-resource.tf
+++ 18-Azure-VM-ScaleSets-Manual-scaling/terraform-manifests/c7-03-web-linux-vmss-resource.tf
@@ -37,7 +37,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
   source_image_reference {
     publisher = "RedHat"
     offer = "RHEL"
-    sku = "83-gen2"
+    #sku = "83-gen2"
+    sku = "8-lvm-gen2"
     version = "latest"
   }   
 
